91754f26f531cba78fe0a37c5a0ef295495ac824
Kristian Skov Johansen <kris701kj@gmail.com>
2024-05-06 08:55:58 +0200

    Added a stackpanel control (simple implementation of the CollectionControl)

---
+++ MonoGame.OpenGL.Formatter/Controls/StackPanelControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.OpenGL.Formatter.Controls
+{
+    public class StackPanelControl : CollectionControl
+    {
+        public StackPanelControl(List<IControl> children)
+        {
+            Children = children;
+        }
+    }
+}
