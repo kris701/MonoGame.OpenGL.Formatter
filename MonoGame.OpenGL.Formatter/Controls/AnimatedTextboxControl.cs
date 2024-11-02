7bf9bf4f3300e0f5592cb8c23c310bf119aad8d4
Kristian Skov <kris701kj@gmail.com>
2024-11-02 17:43:05 +0100

    Added an animated textbox control

---
+++ MonoGame.OpenGL.Formatter/Controls/AnimatedTextboxControl.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.OpenGL.Formatter.Controls.Elements;
+using MonoGame.OpenGL.Formatter.Textures;
+
+namespace MonoGame.OpenGL.Formatter.Controls
+{
+    public class AnimatedTextboxControl : TextboxControl
+    {
+        public TextureSetDefinition TileSet
+        {
+            get => AnimatedElement.TileSet;
+            set
+            {
+                AnimatedElement.TileSet = value;
+                AnimatedElement.Finished = false;
+            }
+        }
+
+        public int Frame
+        {
+            get => AnimatedElement.Frame;
+            set => AnimatedElement.Frame = value;
+        }
+
+        public bool AutoPlay
+        {
+            get => AnimatedElement.AutoPlay;
+            set => AnimatedElement.AutoPlay = value;
+        }
+
+        public AnimatedTileElement AnimatedElement;
+
+        public AnimatedTextboxControl()
+        {
+            AnimatedElement = new AnimatedTileElement(this);
+        }
+
+        public override void Initialize()
+        {
+            AnimatedElement.Initialize();
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            AnimatedElement.Update(gameTime);
+            base.Update(gameTime);
+        }
+    }
+}
