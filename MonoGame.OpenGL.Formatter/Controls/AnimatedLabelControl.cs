bea0727b8f0056023050f91a4a5e47032c65d2bc
Kristian Skov <kris701kj@gmail.com>
2024-11-02 11:37:30 +0100

    Added an animated label control

---
+++ MonoGame.OpenGL.Formatter/Controls/AnimatedLabelControl.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.OpenGL.Formatter.Controls.Elements;
+using MonoGame.OpenGL.Formatter.Textures;
+
+namespace MonoGame.OpenGL.Formatter.Controls
+{
+    public class AnimatedLabelControl : LabelControl
+    {
+        public TextureSetDefinition TileSet
+        {
+            get => AnimatedElement.TileSet;
+            set
+            {
+                AnimatedElement.TileSet = value;
+                AnimatedElement.Finished = false;
+            }
+        }
+        public int Frame
+        {
+            get => AnimatedElement.Frame;
+            set => AnimatedElement.Frame = value;
+        }
+        public bool AutoPlay
+        {
+            get => AnimatedElement.AutoPlay;
+            set => AnimatedElement.AutoPlay = value;
+        }
+        public AnimatedTileElement AnimatedElement;
+
+        public AnimatedLabelControl()
+        {
+            AnimatedElement = new AnimatedTileElement(this);
+        }
+
+        public override void Initialize()
+        {
+            AnimatedElement.Initialize();
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            AnimatedElement.Update(gameTime);
+            base.Update(gameTime);
+        }
+    }
+}
