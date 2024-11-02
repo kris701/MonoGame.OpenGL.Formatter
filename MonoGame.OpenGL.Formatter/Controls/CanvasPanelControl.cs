c03b7f98cbe5043d7e9477e3ae7f23a57528a2f9
Kristian Skov <kris701kj@gmail.com>
2024-11-02 17:19:40 +0100

    Added a canvas panel control

---
+++ MonoGame.OpenGL.Formatter/Controls/CanvasPanelControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.OpenGL.Formatter.Controls
+{
+    public class CanvasPanelControl : CollectionControl
+    {
+        public CanvasPanelControl(List<IControl> children)
+        {
+            Children = children;
+        }
+    }
+}
