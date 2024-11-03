a5510b990863499dd415d1e57b6f948f3765f4f2
Kristian Skov <kris701kj@gmail.com>
2024-11-03 18:11:56 +0100

    Added orientation and Gap to stackpanel

--- MonoGame.OpenGL.Formatter/Controls/StackPanelControl.cs
+++ MonoGame.OpenGL.Formatter/Controls/StackPanelControl.cs
@@ -2,6 +2,11 @@
 {
     public class StackPanelControl : CollectionControl
     {
+        public enum Orientations { Vertical, Horizontal }
+
+        public Orientations Orientation { get; set; } = Orientations.Vertical;
+        public int Gap { get; set; } = 0;
+
         public StackPanelControl(List<IControl> children)
         {
             Children = children;
@@ -12,10 +17,20 @@
             float offset = 0;
             foreach (var child in Children)
             {
-                child.Y += offset;
-                if (child.Width == 0)
-                    child.Width = Width;
-                offset += child.Height;
+                if (Orientation == Orientations.Vertical)
+                {
+                    child.Y += offset;
+                    if (child.Width == 0)
+                        child.Width = Width;
+                    offset += child.Height + Gap;
+                }
+                else
+                {
+                    child.X += offset;
+                    if (child.Height == 0)
+                        child.Height = Height;
+                    offset += child.Width + Gap;
+                }
             }
             base.Initialize();
         }
