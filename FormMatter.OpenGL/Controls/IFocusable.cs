7c63a4acd2a5008896f41bfece3f494a78fa8f12
Kristian Skov <kris701kj@gmail.com>
2026-02-26 16:16:31 +0100

    Updated IFocusable interface

--- FormMatter.OpenGL/Controls/IFocusable.cs
+++ FormMatter.OpenGL/Controls/IFocusable.cs
@@ -1,6 +1,13 @@
 ﻿namespace FormMatter.OpenGL.Controls
 {
+	/// <summary>
+	/// Interface for focusable controls
+	/// </summary>
 	public interface IFocusable
 	{
+		/// <summary>
+		/// Is the control enabled?
+		/// </summary>
+		public bool IsEnabled { get; set; }
 	}
 }
